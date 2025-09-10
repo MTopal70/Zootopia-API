bfc8156286102209fa145530ec1c009cf7ab9aea
MTopal70 <muharrem.topal@outlook.com>
2025-09-10 19:41:10 +0200

    Added .env file support and secured API key

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,12 +1,13 @@
+import os
+from dotenv import load_dotenv
 import requests
 
-API_KEY = "4H21ggbBhTMC1opbfp8FVQ==Bc3M4B0xYTu9ofQw"
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 API_URL = "https://api.api-ninjas.com/v1/animals?name="
 
 def fetch_data(animal_name):
-    """Gets the animals data for the animal 'animal_name'.
-        Returns: a list of animals, each animal is a dictionary.
-    """
     headers = {"X-Api-Key": API_KEY}
     response = requests.get(API_URL + animal_name, headers=headers)
 
@@ -15,3 +16,4 @@ def fetch_data(animal_name):
     else:
         print(f"Error fetching data: {response.status_code}")
         return []
+
