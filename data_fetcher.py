2ab744a5f216751411fe74a3af44e25ce61f8499
MTopal70 <muharrem.topal@outlook.com>
2025-09-11 23:35:12 +0200

    Unused Imports removed and commented API Key deleted

--- data_fetcher.py
+++ data_fetcher.py
@@ -8,6 +8,7 @@ API_KEY = os.getenv("API_KEY")
 API_URL = "https://api.api-ninjas.com/v1/animals?name="
 
 def fetch_data(animal_name):
+    """API call for animals with specific amen.  API Key stored in secure way in .env file."""
     headers = {"X-Api-Key": API_KEY}
     response = requests.get(API_URL + animal_name, headers=headers)
 
